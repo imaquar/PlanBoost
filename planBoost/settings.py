6708eabaa0f7f4325d1d2ce5f391e82267d89493
Ranel Tagirov <raneltagirov06@mail.ru>
2026-01-20 14:00:12 +0300

    register project apps in settings

--- planBoost/settings.py
+++ planBoost/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'notes.apps.NotesConfig',
+    'tasks.apps.TasksConfig',
+    'timer.apps.TimerConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
