f84bda31659c858fe9dd7fbdf9e9def2934bdfff
Ranel Tagirov <rntagirov@edu.hse.ru>
2026-04-07 20:35:49 +0300

    add recent notes query for dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,8 +2,10 @@ from django.template.response import TemplateResponse
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from tasks.models import Task
+from notes.models import Note
 
 @login_required
 def index(request):
     upcoming_tasks = (Task.objects.filter(user=request.user, status=False, deadline__gte=timezone.now(),).order_by('deadline')[:3])
-    return TemplateResponse(request,'dashboard/dashboard.html', {'upcoming_tasks': upcoming_tasks},)
+    recent_notes = (Note.objects.filter(user=request.user).order_by('-created_at')[:3])
+    return TemplateResponse(request,'dashboard/dashboard.html', {'upcoming_tasks': upcoming_tasks, 'recent_notes': recent_notes},)
