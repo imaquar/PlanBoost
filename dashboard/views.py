36810a4473b32a43c36dc3c37b9328c32245d915
Ranel Tagirov <rntagirov@edu.hse.ru>
2026-04-07 15:35:56 +0300

    remove unused dashboard views

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,12 +2,3 @@ from django.template.response import TemplateResponse
 
 def index(request):
     return TemplateResponse(request, 'dashboard/dashboard.html')
-
-def notes(request):
-    return TemplateResponse(request, 'notes/notes.html')
-
-def tasks(request):
-    return TemplateResponse(request, 'tasks/tasks.html')
-
-def timer(request):
-    return TemplateResponse(request, 'timer/timer.html')
