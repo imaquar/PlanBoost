b0c8ee7a4f21da21bb733688ab1640c4102b524c
Ranel Tagirov <rntagirov@edu.hse.ru>
2026-04-07 20:20:43 +0300

    add upcoming tasks query for dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,9 @@
 from django.template.response import TemplateResponse
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from tasks.models import Task
 
 @login_required
 def index(request):
-    return TemplateResponse(request, 'dashboard/dashboard.html')
+    upcoming_tasks = (Task.objects.filter(user=request.user, status=False, deadline__gte=timezone.now(),).order_by('deadline')[:3])
+    return TemplateResponse(request,'dashboard/dashboard.html', {'upcoming_tasks': upcoming_tasks},)
