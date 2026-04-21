1c8d64585620f04a1d94ae7129b43c7b66628eaa
Ranel Tagirov <rntagirov@edu.hse.ru>
2026-04-21 11:36:29 +0300

    add user login and logout tests

--- users/tests.py
+++ users/tests.py
@@ -36,3 +36,38 @@ class UserRegistrationFormTests(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('users:login'))
         self.assertTrue(User.objects.filter(username='newuser02').exists())
+
+
+class UserLoginLogoutTests(TestCase):
+    def setUp(self):
+        self.username = 'authuser01'
+        self.password = 'StrongPass123'
+        self.user = User.objects.create_user(username=self.username, password=self.password,)
+
+    def test_login_with_correct_credentials(self):
+        response = self.client.post(
+            reverse('users:login'),
+            data={'username': self.username, 'password': self.password,},
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('dashboard:dashboard'))
+        self.assertIn('_auth_user_id', self.client.session)
+
+    def test_login_with_incorrect_password(self):
+        response = self.client.post(
+            reverse('users:login'),
+            data={'username': self.username, 'password': 'WrongPassword999',},
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertNotIn('_auth_user_id', self.client.session)
+
+    def test_logout_ends_authenticated_session(self):
+        self.client.login(username=self.username, password=self.password)
+
+        response = self.client.post(reverse('logout'))
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/users/login/')
+        self.assertNotIn('_auth_user_id', self.client.session)
