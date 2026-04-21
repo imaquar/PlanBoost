83d6d135ac9b5392a5267d8b5aa8c8cfc7e91550
Ranel Tagirov <rntagirov@edu.hse.ru>
2026-04-21 11:20:19 +0300

    add user registration form tests

--- users/tests.py
+++ users/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth.models import User
+
+from .forms import RegisterForm
 
 
 class AuthenticationPagesTests(TestCase):
@@ -14,3 +17,22 @@ class AuthenticationPagesTests(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'registration/register.html')
+
+
+class UserRegistrationFormTests(TestCase):
+    def test_register_form_is_valid_with_correct_data(self):
+        form = RegisterForm(
+            data={'username': 'newuser01', 'password1': 'StrongPass123', 'password2': 'StrongPass123',}
+        )
+
+        self.assertTrue(form.is_valid(), form.errors)
+
+    def test_register_post_creates_user_with_correct_data(self):
+        response = self.client.post(
+            reverse('users:register'),
+            data={'username': 'newuser02', 'password1': 'StrongPass123', 'password2': 'StrongPass123',},
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('users:login'))
+        self.assertTrue(User.objects.filter(username='newuser02').exists())
