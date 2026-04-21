d8d7b1c028254cc52cfd7a67f04e1b1aac373a85
Ranel Tagirov <rntagirov@edu.hse.ru>
2026-04-21 11:08:47 +0300

    add user authentication pages tests

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class AuthenticationPagesTests(TestCase):
+    def test_login_page_is_available_and_uses_template(self):
+        response = self.client.get(reverse('users:login'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/login.html')
+
+    def test_register_page_is_available_and_uses_template(self):
+        response = self.client.get(reverse('users:register'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/register.html')
