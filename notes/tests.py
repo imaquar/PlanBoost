07dbae37bf1373cd7a6cad21fce8587877f589db
Ranel Tagirov <rntagirov@edu.hse.ru>
2026-04-21 12:33:24 +0300

    add notes list view tests

--- notes/tests.py
+++ notes/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 from .models import Note
 
@@ -20,3 +21,29 @@ class NoteModelTests(TestCase):
         note = Note.objects.create(label='Some title', text='Some text', user=self.user,)
 
         self.assertEqual(str(note), 'Some title')
+
+
+class NotesListViewTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='notesowner', password='StrongPass123',)
+        self.other_user = User.objects.create_user(username='othernotes', password='StrongPass123',)
+
+    def test_authenticated_user_can_open_notes_list(self):
+        self.client.login(username='notesowner', password='StrongPass123')
+
+        response = self.client.get(reverse('notes:notes'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'notes/notes.html')
+
+    def test_notes_list_contains_only_current_user_notes(self):
+        own_note = Note.objects.create(label='Owner note', text='Owner text', user=self.user,)
+        Note.objects.create(label='Other note', text='Other text', user=self.other_user,)
+        self.client.login(username='notesowner', password='StrongPass123')
+
+        response = self.client.get(reverse('notes:notes'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(own_note, response.context['note'])
+        self.assertContains(response, 'Owner note')
+        self.assertNotContains(response, 'Other note')
