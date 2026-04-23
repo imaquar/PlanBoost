a1694a29878656f0044b6b4f2ccf5fad1abdd0f9
Ranel Tagirov <rntagirov@edu.hse.ru>
2026-04-23 15:40:56 +0300

    add tasks list view tests

--- tasks/tests.py
+++ tasks/tests.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 
 from datetime import timedelta
 from .models import Task
+from django.urls import reverse
 
 
 class TaskModelTests(TestCase):
@@ -37,3 +38,33 @@ class TaskModelTests(TestCase):
             deadline=timezone.now() + timedelta(days=3), user=self.user,)
 
         self.assertEqual(str(task), 'Readable task title')
+
+
+class TasksListViewTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(username='tasks_owner', password='testpass123',)
+        self.other_user = get_user_model().objects.create_user(username='tasks_other', password='testpass123',)
+
+        self.own_task = Task.objects.create(label='Owner task', description='Owner description', priority=2,
+            deadline=timezone.now() + timedelta(days=1), user=self.user,)
+        self.other_task = Task.objects.create(label='Other task', description='Other description', priority=1,
+            deadline=timezone.now() + timedelta(days=2), user=self.other_user,)
+
+    def test_authenticated_user_can_open_tasks_list(self):
+        self.client.login(username='tasks_owner', password='testpass123')
+
+        response = self.client.get(reverse('tasks:tasks'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'tasks/tasks.html')
+
+    def test_tasks_list_shows_only_current_user_tasks(self):
+        self.client.login(username='tasks_owner', password='testpass123')
+
+        response = self.client.get(reverse('tasks:tasks'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(self.own_task, response.context['task'])
+        self.assertNotIn(self.other_task, response.context['task'])
+        self.assertContains(response, 'Owner task')
+        self.assertNotContains(response, 'Other task')
